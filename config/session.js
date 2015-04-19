61486bc8eaf2a69bfeb2b98f225e766943603424
Felix Milea-Ciobanu <felixmilea@gmail.com>
2015-04-18 18:46:43 -0600

    session

--- config/session.js
+++ config/session.js
@@ -14,43 +14,23 @@
 
 module.exports.session = {
 
-  /***************************************************************************
-  *                                                                          *
-  * Session secret is automatically generated when your new app is created   *
-  * Replace at your own risk in production-- you will invalidate the cookies *
-  * of your users, forcing them to log in again.                             *
-  *                                                                          *
-  ***************************************************************************/
-  secret: 'd940704cbfdaa914ab138613cb9aaffd',
+  secret: 'a940078cbfd9a914ab138613cb9aaffd',
 
+	rolling:  true,
+	resave:   false,
+	proxy:    true,
+	saveUnitialized: false,
 
-  /***************************************************************************
-  *                                                                          *
-  * Set the session cookie expire time The maxAge is set by milliseconds,    *
-  * the example below is for 24 hours                                        *
-  *                                                                          *
-  ***************************************************************************/
+	cookie: {
+		maxAge: 24 * 60 * 60 * 1000,
+		httpOnly: true
+		//secure:   true,
+	},
 
-  // cookie: {
-  //   maxAge: 24 * 60 * 60 * 1000
-  // },
+	adapter:  'redis',
+//	hostname: repi.config.persistence.redis.hostname,
+//	port:     repi.config.persistence.redis.port
 
-  /***************************************************************************
-  *                                                                          *
-  * In production, uncomment the following lines to set up a shared redis    *
-  * session store that can be shared across multiple Sails.js servers        *
-  ***************************************************************************/
-
-  // adapter: 'redis',
-
-  /***************************************************************************
-  *                                                                          *
-  * The following values are optional, if no options are set a redis         *
-  * instance running on localhost is expected. Read more about options at:   *
-  * https://github.com/visionmedia/connect-redis                             *
-  *                                                                          *
-  *                                                                          *
-  ***************************************************************************/
 
   // host: 'localhost',
   // port: 6379,
@@ -59,33 +39,5 @@ module.exports.session = {
   // pass: <redis auth password>,
   // prefix: 'sess:',
 
-
-  /***************************************************************************
-  *                                                                          *
-  * Uncomment the following lines to use your Mongo adapter as a session     *
-  * store                                                                    *
-  *                                                                          *
-  ***************************************************************************/
-
-  // adapter: 'mongo',
-  // host: 'localhost',
-  // port: 27017,
-  // db: 'sails',
-  // collection: 'sessions',
-
-  /***************************************************************************
-  *                                                                          *
-  * Optional Values:                                                         *
-  *                                                                          *
-  * # Note: url will override other connection settings url:                 *
-  * 'mongodb://user:pass@host:port/database/collection',                     *
-  *                                                                          *
-  ***************************************************************************/
-
-  // username: '',
-  // password: '',
-  // auto_reconnect: false,
-  // ssl: false,
-  // stringify: true
 
 };
