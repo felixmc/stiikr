e511add03a8895ede5462d8525ea6d2f734ec133
Felix Milea-Ciobanu <felixmilea@gmail.com>
2015-04-19 00:44:37 -0600

    voting logic

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
 	$(document.body).on('click', '.upvote', function() {
 		var $this = $(this);
 		var $post = $this.closest('.post');
+		console.log($post);
 		$.post({
 			url: '/upvote/' + $post.attr('data-id')
 		}, function(data) {
