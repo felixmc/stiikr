d17c5257972adca77aa8f01d3bbbfc3f26354b13
Felix Milea-Ciobanu <felixmilea@gmail.com>
2015-04-18 23:43:46 -0600

    voting logic

--- api/controllers/MainController.js
+++ api/controllers/MainController.js
@@ -1,3 +1,48 @@
+function handleVote(req, res, voteValue) {
+	if (req.method === 'POST' || req.session.authenticated) {
+		Post.find( req.param('id') )
+		.populate('votes')
+		.exec(function(err, post) {
+			if (err)
+				sails.log.error(err);
+
+			if (post) {
+				post = post[0];
+				console.log(post);
+					
+				var vote = _.find(post.votes, { user: req.session.user.id });
+
+				if (vote) {
+					var newValue = vote.value == voteValue ? 0 : voteValue;
+					Vote.update(vote.id, { value: newValue })
+					.exec(function(err, vote) {
+						if (err) {
+							sails.log.error(err);
+							res.serverError();
+						} else
+							res.send('vote successfully updated');
+					});
+				} else {
+					Vote.create({
+						user:  req.session.user.id,
+						post:  post.id,
+						value: voteValue
+					}).exec(function(err, vote) {
+						if (err) {
+							sails.log.error(err);
+							res.serverError();
+						} else
+							res.send('vote successfully created');
+					});
+				}
+			} else {
+				res.notFound();			
+			}
+		});			
+	} else {
+		res.notFound();
+	}
+}
 
 // Main Controller
 module.exports = {
@@ -14,58 +59,11 @@ module.exports = {
 	},
 	
 	upvote: function(req, res) {
-		if (req.method === 'POST' || req.session.authenticated) {
-			Post.find( req.param('id') )
-			.populate('votes')
-			.exec(function(err, post) {
-				if (err)
-					sails.log.error(err);
-
-				if (post) {
-					post = post[0];
-					console.log(post);
-					
-					var vote = _.find(post.votes, { user: req.session.user.id });
-
-					if (vote) {
-						Vote.update(vote.id, { value: vote.value == 1 ? 0 : 1 })
-						.exec(function(err, vote) {
-							if (err) {
-								sails.log.error(err);
-								res.serverError();
-							} else
-								res.send('vote successfully updated');
-						});
-					} else {
-						console.log({
-							user:  req.session.user.id,
-							post:  post.id,
-							value: 1
-						});
-						Vote.create({
-							user:  req.session.user.id,
-							post:  post.id,
-							value: 1
-						}).exec(function(err, vote) {
-							if (err) {
-								sails.log.error(err);
-								res.serverError();
-							} else
-								res.send('vote successfully created');
-						});
-					}
-				} else {
-					res.notFound();			
-				}
-			});
-			
-		} else {
-			res.notFound();
-		}
+		handleVote(req, res, 1);
 	},
 	
 	downvote: function(req, res) {
-	
+		handleVote(req, res, -1);	
 	}, 
 	
 	new: function(req, res) {
