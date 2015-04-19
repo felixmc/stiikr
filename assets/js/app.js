85dd7ce7ce326d622361b0e676d3f4792e80ab14
Felix Milea-Ciobanu <felixmilea@gmail.com>
2015-04-19 02:51:58 -0600

    voting logic

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,15 +9,35 @@ $(document).ready(function() {
 		var url = '/upvote/' + $post.attr('data-id');
 		console.log(url);
 		
+		
+		//var socket = io.connect('http://localhost:1337');
+
+//		io.socket.on('connect', function () {
+
+			// Request data from '/user' route over socket
+			// The second argument is an arbitrary set of data
+			// Here, If we haven't overridden the scaffold, and assuming you have a user model,
+			// socket.request() will respond with the first 3 users from your database.
+			// This will also automatically subscribe us to realtime updates for those 3 users
+			// as well as any new users added to the collection.
+			io.socket.request(url, {
+				limit: 3
+			}, function (response) {
+				// Here's what the server responded with
+				console.log(response);
+			});
+//		});
+		
+		
 //		console.log('clicked!');
-		var ajax = $.post(url, function(data, status) {
-			console.log('success!');
+		//var ajax = $.post(url, function(data, status) {
+			//console.log('success!');
 			//console.log(data);
 				//$('.score', $post).text(data);
 				//$post.removeClass('downvoted').toggleClass('upvoted');
-		});
+		//});
 		
-		console.log(ajax);
+		//console.log(ajax);
 		
 		e.preventDefault();
 		e.stopPropagation();
