a32a276b0b224f9c70a316d499d04eb0e38ad116
Felix Milea-Ciobanu <felixmilea@gmail.com>
2015-04-18 18:15:05 -0600

    auth

--- api/controllers/MainController.js
+++ api/controllers/MainController.js
@@ -5,8 +5,15 @@ module.exports = {
 	index: function(req, res) {
 	
 //		console.log(req.session.user);
-		
+
 		res.render('home', { user: req.session.user });
+	},
+	
+	new: function(req, res) {
+
+		console.log(req.method);
+		
+		res.send("new");
 	}
 	
 };
