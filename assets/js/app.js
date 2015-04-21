98db192f68189ca65456053e481d4c7cc073476c
Felix Milea-Ciobanu <felixmilea@gmail.com>
2015-04-20 20:56:57 -0600

    css start

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,30 +4,41 @@ $(document).ready(function() {
 
 	$("time.timeago").timeago();	
 	
+	var voteChosenClass = 'chosen';
+	
 	// vote handling
 	$(document.body).on('click', '.vote-button', function(e) {
 		var $this = $(this);
 		var $post = $this.closest('.post');
 		
 		var url = '/'+$this.attr('data-action')+'/' + $post.attr('data-id');
-		console.log(url);
+//		console.log(url);
 		
 		io.socket.request({
 			url: url,
 			method: 'POST'
 		}, function (response) {
-			console.log('resp: ' + response);
+//			console.log('resp: ' + response);
 		});
 		
 		e.preventDefault();
 		e.stopPropagation();
+
+		if ($this.hasClass(voteChosenClass)) {
+			$this.removeClass(voteChosenClass);
+		} else {
+			$('.vote-button', $post).removeClass(voteChosenClass);
+			$this.addClass(voteChosenClass);
+		}
 		
 		return false;
 	});
 	
 	io.socket.on('voteUpdate', function(data) {
-		$('.post[data-id="' + data.post + '"] .score-box').attr('data-score', data.score);
-		console.log(data);
+		var $post = $('.post[data-id="' + data.post + '"]');
+		
+		$('.score-box', $post).attr('data-score', data.score);
+//		console.log(data);
 	});
 	
 	
