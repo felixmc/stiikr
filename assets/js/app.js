f6d738906ae63d47e8fbbfea78de84fa91b87e98
Felix Milea-Ciobanu <felixmilea@gmail.com>
2015-04-25 07:59:58 -0600

    voting limitations logic

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,38 +2,38 @@
 
 $(document).ready(function() {
 
-	$("time.timeago").timeago();	
-	
+	$("time.timeago").timeago();
+
 	// login form nonsense
 	$(document.body).on('click', '.throw', function(e) {
 		var $this   = $(this);
 		var $header = $this.closest('.header');
-		
+
 		if ($header.hasClass('expanded')) {
 			$this.closest('form').submit();
 		} else {
 			$header.addClass('expanded');
 		}
-		
+
 		e.preventDefault();
 		e.stopPropagation();
-		
+
 		return false;
 	});
-	
-	
-	
-	
+
+
+
+
 	var voteChosenClass = 'chosen';
-	
+
 	// vote handling
-	$(document.body).on('click', '.vote-button', function(e) {
+	$(document.body).on('click', '.post:not(.stale, .locked) .vote-button', function(e) {
 		var $this = $(this);
 		var $post = $this.closest('.post');
-		
+
 		var url = '/'+$this.attr('data-action')+'/' + $post.attr('data-id');
 //		console.log(url);
-		
+
 		io.socket.request({
 			url: url,
 			method: 'POST'
@@ -45,19 +45,19 @@ $(document).ready(function() {
 			$('.vote-button', $post).removeClass(voteChosenClass);
 		}
 		$this.toggleClass(voteChosenClass);
-		
+
 		e.preventDefault();
 		e.stopPropagation();
-		
+
 		return false;
 	});
-	
+
 	io.socket.on('voteUpdate', function(data) {
 		var $post = $('.post[data-id="' + data.post + '"]');
-		
+
 		$('.score-box', $post).attr('data-score', data.score);
 //		console.log(data);
 	});
-	
-	
+
+
 });
