56989b3279387079943fb42c425d374c99ef17c1
Felix Milea-Ciobanu <felixmilea@gmail.com>
2015-04-27 00:32:51 -0600

    fix

--- api/services/DateService.js
+++ api/services/DateService.js
@@ -2,7 +2,7 @@
 
 var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-module.exports = function() {
+module.exports = {
 	month: function(month) {
 		return months[month];
 	}
